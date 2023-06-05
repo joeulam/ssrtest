c7cb8ebe725d928f9f5f77c4db840dcbb7f1e6cc
Joey Lam <joeulam345@gmail.com>
2023-06-04 23:01:02 -0400

    w

--- app/lib.js
+++ app/lib.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-export async function Tst() {
+export default async function Tst() {
   const music = await loadspotify();
   //var a = music.entries[1].API
   if(music.is_playing){
